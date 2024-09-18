355613f9e16157e12da310a55ac1e0cf534e16e2
Faizan <faizanahmad8369@gmail.com>
2024-09-18 13:29:56 +0530

    initail commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  //you can add other file types in the content, for example if you us typescript, you can ts, or jsx, or txs
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
